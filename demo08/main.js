9a7250aaee440549d8ee2ec602529022b5c6fd8d
jaysoncheng(程金山) <jaysoncheng@tencent.com>
2019-03-17 15:45:54 +0800

    修改webpack配置

--- demo08/main.js
+++ demo08/main.js
@@ -1,1 +1,1 @@
-document.write('<h1>Hello World</h1>');
+document.write('<h1>Helloasdasd World</h1>');
--- demo15/webpack.config.js
+++ demo15/webpack.config.js
@@ -1,3 +1,4 @@
+const path = require("path")
 module.exports = {
   entry: './index.js',
   output: {
@@ -20,5 +21,25 @@ module.exports = {
         }
       },
     ]
+  },
+  devServer: {
+    // 在此文件夹下启动服务，可以直接访问html文件，但是js在开发模式是访问的内存中的
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,// gzip压缩
+    port: 9000,
+    // hot:true,
+    open:true,//命令行可以--open
+    openPage: 'index.html',
+    allowedHosts: [     // 请求白名单列表
+      'subdomain.host.com',
+      'subdomain2.host.com',
+      '.host.com',
+      'host2.com',
+    ],
+    before: function(app, server) {
+      app.get('/aaaa', function(req, res) {
+        res.json({ custom: 'response' });
+      });
+    }
   }
 };
